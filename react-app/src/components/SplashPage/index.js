1fa9495cdd61bbe505719da8187a6e14d2c96587
lamtran415 <austin415@ymail.com>
2023-07-14 10:10:49 -0700

    Fixed Splash Page

--- react-app/src/components/SplashPage/index.js
+++ react-app/src/components/SplashPage/index.js
@@ -12,26 +12,27 @@ function importAll(context) {
 
 const images = importAll(require.context('./SplashPhotos', false, /\.(png|jpe?g|svg)$/));
 const backgroundArr = Object.values(images).map((image) => `url(${image.default})`);
+console.log(backgroundArr)
+{backgroundArr.map((background, index) => (
+    console.log(index)
+
+    ))}
 
 const SplashPage = () => {
     const history = useHistory();
-    const [backgroundImage, setBackgroundImage] = useState(0);
-
-    const changeBackground = useCallback(() => {
-        setBackgroundImage((prevNum) => ++prevNum % backgroundArr.length);
-    }, [])
-
-    // eslint-disable-next-line
-    useEffect(() => {
-        const backgroundInterval = setInterval(changeBackground, 5000)
-
-        return () => {
-            clearInterval(backgroundInterval)
-        }
-    },[changeBackground])
 
     return (
-        <div className="whole-splash-page-container" style={{"backgroundImage": backgroundArr[backgroundImage]}}>
+        <div className="whole-splash-page-container">
+            <div className="image-slides-container">
+                {backgroundArr.map((background, index) => (
+                <div
+                    key={`image-slide${index}`}
+                    className="image-slide"
+                    id={`image-slide${backgroundArr.length - index}`}
+                    style={{ backgroundImage: background }}
+                />
+                ))}
+            </div>
             <div className="center-page-info">
                 <div className="inspiration-header">Find your inspiration.</div>
                 <div className="ballr-community">Join the Ballr community, home to tens of billions of photos and 2 million groups</div>
