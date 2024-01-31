d314b478f116534f99ed5c2fe3dd5ff3622fdf5f
lamtran415 <austin415@ymail.com>
2024-01-31 15:14:07 -0800

    Created favorites route template and added to init

--- app/api/favorites_routes.py
+++ app/api/favorites_routes.py
@@ -1,9 +1,21 @@
 from flask import Blueprint, jsonify, request, session, redirect
 from flask_login import login_required, current_user
 from app.models import Photo, Comment, db
-from app.forms.photo_form import PhotoForm
-from app.forms.comment_form import CommentForm
 from .auth_routes import validation_errors_to_error_messages
 
+favorites_routes = Blueprint('favorites_routes', __name__)
 
 
+# Add Favorite
+# As a logged-in user, I want to add photo to favorites
+# POST /api/favorites/users/:userId
+
+
+# View Favorites
+# As a logged-in user, I want to favorites of a user.
+# GET /api/favorites/users/:userId
+
+
+# Delete Favorite
+# As a logged-in, I want to delete a photo from favorites
+# DELETE /api/favorites/:favoriteId/users/:userId
